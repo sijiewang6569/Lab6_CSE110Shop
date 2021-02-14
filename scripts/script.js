cc7f241b5ed7c67a1d8ef970117858ddda22982c
Sijie Wang <56042391+sijiewang6569@users.noreply.github.com>
2021-02-14 01:17:57 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,41 @@
 // Script.js
 
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
-  if(localStorage.getItem('items') == null) {
-    fetch('https://fakestoreapi.com/products')
+  
+  if (localStorage.getItem("data") == null) {
+    fetch("https://fakestoreapi.com/products")
       .then(response => response.json())
-      .then(data => localStorage.setItem('items', JSON.stringify(data)));
+      .then(data => localStorage.setItem("data", JSON.stringify(data)));
   }
 
-  if(localStorage.getItem('cart') == null) {
+  let products = JSON.parse(localStorage.getItem("data"));
+  let productList = document.getElementById("product-list");
+  let cartCount = document.getElementById("cart-count");
+
+  if (localStorage.getItem("cart") == null) {
     let cart = [];
-    localStorage.setItem('cart', JSON.stringify(cart));
+    localStorage.setItem("cart", JSON.stringify(cart));
   }
-  cart = JSON.parse(localStorage.getItem('cart'));
+  cart = JSON.parse(localStorage.getItem("cart"));
 
-  let cartCount = document.getElementById('cart-count');
-  if(localStorage.getItem('sizeOfCart') == null) {
-    localStorage.setItem('sizeOfCart', JSON.stringify(cartCount.textContent));
+  if(JSON.parse(localStorage.getItem('count')) == null) {
+    localStorage.setItem('count', JSON.stringify(cartCount.textContent));
+    count = JSON.parse(localStorage.getItem('count'))
+  } else {
+    count = JSON.parse(localStorage.getItem('count'));
   }
-  cartCount.textContent = JSON.parse(localStorage.getItem('sizeOfCart'));
+  cartCount.textContent = count;
 
-  let dataStored = JSON.parse(localStorage.getItem('items'));
-  let productsList = document.getElementById('products-list');
+  /*for (var product in products) {
+    let item = products[product];
+    let productItem = document.createElement("product-item");
+    productItem = new ProductItem(item.id, item.image, item.title, item.price, cart.includes(item.id));
+    productList.appendChild(ProductItem);
+  }*/
 
-  if (dataStored != null){
-    dataStored.forEach(function(index){
-      let curr = document.createElement('product-item');
-      curr = new ProductItem(index.imagesrc, index.title, index.price, index.id, cart.includes(index.id));
-      if (productsList != null){
-        productsList.appendChild(curr);
-      }
-    });
-  }
+  products.forEach(function (product) {
+    let el = document.createElement('product-item');
+    el = new ProductItem(product.id, product.image, product.title, product.price, cart.includes(product.id));
+    productList.appendChild(el);
+  });
 });
