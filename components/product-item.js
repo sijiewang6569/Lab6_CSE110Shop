19986e898c48f01b9ac788da07ce62b90c33f62b
Sijie Wang <56042391+sijiewang6569@users.noreply.github.com>
2021-02-13 20:53:23 -0800

    Update product-item.js

--- components/product-item.js
+++ components/product-item.js
@@ -1,106 +1,119 @@
 class ProductItem extends HTMLElement {
-  constructor() {
+
+  constructor(imagesrc, title, price, id, addOrRemove) {
+
     super();
-    // Create a shadow root
-    this.attachShadow({mode: 'open'}); // sets and returns 'this.shadowRoot'
+    this.attachShadow( { mode: "open" });
+    
+    const li = this.shadowRoot.appendChild(document.createElement('li'));
+    const img = document.createElement('img');
+    const price = document.createElement('p');
+    const title = document.createElement('P');
+    const button = document.createElement('button');
 
-    const li = document.createElement('li');
     li.setAttribute('class', 'product');
-
-    const img = li.appendChild(document.createElement('img'));
-
-    const title = li.appendChild(document.createElement('p'));
+    img.setAttribute('src', imagesrc);
+    img.setAttribute('alt', title);
+    title.textContent = title;
+    price.textContent = "$" + price;
     title.setAttribute('class', 'title');
-
-    const price = li.appendChild(document.createElement('p'));
     price.setAttribute('class', 'price');
+    button.textContent = "Add to Cart";
+    if(addOrRemove) {
+      button.textContent = 'Remove from Cart';
+    }
 
-    const button = li.appendChild(document.createElement('button'));
-    button.innerHTML = "Add to Cart";
-    var cartID = button.parentElement.dataset.cartID;
-    button.addEventListener("click", clickButton);
-    function clickButton() {
-        if (button.innerHTML == "Add to Cart") {
+    li.appendChild(img);
+    li.appendChild(price);
+    li.appendChild(title);
+    li.appendChild(button);
+
+    this.shadowRoot.innerHTML = 
+      `<style>
+        .price {
+          color: green;
+          font-size: 1.8em;
+          font-weight: bold;
+          margin: 0;
+        }
+        
+        .product {
+          align-items: center;
+          background-color: white;
+          border-radius: 5px;
+          display: grid;
+          grid-template-areas: 
+          "image"
+          "title"
+          "price"
+          "add";
+          grid-template-rows: 67% 11% 11% 11%;
+          height: 450px;
+          filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
+          margin: 0 30px 30px 0;
+          padding: 10px 20px;
+          width: 200px;
+        }
+        
+        .product > button {
+          background-color: rgb(255, 208, 0);
+          border: none;
+          border-radius: 5px;
+          color: black;
+          justify-self: center;
+          max-height: 35px;
+          padding: 8px 20px;
+          transition: 0.1s ease all;
+        }
+        
+        .product > button:hover {
+          background-color: rgb(255, 166, 0);
+          cursor: pointer;
+          transition: 0.1s ease all;
+        }
+        
+        .product > img {
+          align-self: center;
+          justify-self: center;
+          width: 100%;
+        }
+        
+        .title {
+          font-size: 1.1em;
+          margin: 0;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+        }
+        
+        .title:hover {
+          font-size: 1.1em;
+          margin: 0;
+          white-space: wrap;
+          overflow: auto;
+          text-overflow: unset;
+        }
+      </style>`;
+
+      button.onclick = function () {
+        var sizeOfCart = doucment.getElementById('cart-count');
+        var cart = JSON.parse(localStorage.getElementById('cart'));
+        if(button.textContent == "Add to Cart") {
+          button.textContent = "Remove from Cart";
+          sizeOfCart.textContent = parseInt(sizeOfCart.textContent) + 1;
+          cart.push(id);
           alert("Added to Cart!");
-          button.innerHTML = "Remove from Cart";
-          cart.add(cartID);
         }
         else {
+          button.textContent = "Add to Cart";
+          sizeOfCart.textContent = parseInt(sizeOfCart.textContent) - 1;
+          cart.splice(cart.indexOf(id), 1);
           alert("Removed from Cart!");
-          button.innerHTML = "Add to Cart";
-          cart.delete(cartID);
         }
-        cartCount.innerHTML = cart.size;
-    }
-    const style = this.shadowRoot.appendChild(document.createElement('style'));
-    style.innerHTML = 
-    `
-    .price {
-      color: green;
-      font-size: 1.8em;
-      font-weight: bold;
-      margin: 0;
-    }
-    
-    .product {
-      align-items: center;
-      background-color: white;
-      border-radius: 5px;
-      display: grid;
-      grid-template-areas: 
-      'image'
-      'title'
-      'price'
-      'add';
-      grid-template-rows: 67% 11% 11% 11%;
-      height: 450px;
-      filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
-      margin: 0 30px 30px 0;
-      padding: 10px 20px;
-      width: 200px;
-    }
-    
-    .product > button {
-      background-color: rgb(255, 208, 0);
-      border: none;
-      border-radius: 5px;
-      color: black;
-      justify-self: center;
-      max-height: 35px;
-      padding: 8px 20px;
-      transition: 0.1s ease all;
-    }
-    
-    .product > button:hover {
-      background-color: rgb(255, 166, 0);
-      cursor: pointer;
-      transition: 0.1s ease all;
-    }
-    
-    .product > img {
-      align-self: center;
-      justify-self: center;
-      width: 100%;
-    }
-    
-    .title {
-      font-size: 1.1em;
-      margin: 0;
-      overflow: hidden;
-      text-overflow: ellipsis;
-      white-space: nowrap;
-    }
-    
-    .title:hover {
-      font-size: 1.1em;
-      margin: 0;
-      white-space: wrap;
-      overflow: auto;
-      text-overflow: unset;
-    }
-    ` 
+        localStorage.setItem('cart', JSON.stringify(cart));
+        localStorage.setItem('sizeOfCart', JSON.stringify(sizeOfCart));
+      }
   }
-  
 }
 
-customElements.define('product-item', ProductItem);
+customElements.define("product-item", ProductItem);
