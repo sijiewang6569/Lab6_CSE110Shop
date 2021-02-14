0f37123a47a1d55554069a8cd11fc3c45c444b58
Sijie Wang <56042391+sijiewang6569@users.noreply.github.com>
2021-02-13 19:25:37 -0800

    Update product-item.js

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,106 @@
-// product-item.js
-
 class ProductItem extends HTMLElement {
-  // TODO
+  constructor() {
+    super();
+    // Create a shadow root
+    this.attachShadow({mode: 'open'}); // sets and returns 'this.shadowRoot'
+
+    const li = document.createElement('li');
+    li.setAttribute('class', 'product');
+
+    const img = li.appendChild(document.createElement('img'));
+
+    const title = li.appendChild(document.createElement('p'));
+    title.setAttribute('class', 'title');
+
+    const price = li.appendChild(document.createElement('p'));
+    price.setAttribute('class', 'price');
+
+    const button = li.appendChild(document.createElement('button'));
+    button.innerHTML = "Add to Cart";
+    var cartID = button.parentElement.dataset.cartID;
+    button.addEventListener("click", clickButton);
+    function clickButton() {
+        if (button.innerHTML == "Add to Cart") {
+          alert("Added to Cart!");
+          button.innerHTML = "Remove from Cart";
+          cart.add(cartID);
+        }
+        else {
+          alert("Removed from Cart!");
+          button.innerHTML = "Add to Cart";
+          cart.delete(cartID);
+        }
+        cartCount.innerHTML = cart.size;
+    }
+    const style = this.shadowRoot.appendChild(document.createElement('style'));
+    style.innerHTML = 
+    `
+    .price {
+      color: green;
+      font-size: 1.8em;
+      font-weight: bold;
+      margin: 0;
+    }
+    
+    .product {
+      align-items: center;
+      background-color: white;
+      border-radius: 5px;
+      display: grid;
+      grid-template-areas: 
+      'image'
+      'title'
+      'price'
+      'add';
+      grid-template-rows: 67% 11% 11% 11%;
+      height: 450px;
+      filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
+      margin: 0 30px 30px 0;
+      padding: 10px 20px;
+      width: 200px;
+    }
+    
+    .product > button {
+      background-color: rgb(255, 208, 0);
+      border: none;
+      border-radius: 5px;
+      color: black;
+      justify-self: center;
+      max-height: 35px;
+      padding: 8px 20px;
+      transition: 0.1s ease all;
+    }
+    
+    .product > button:hover {
+      background-color: rgb(255, 166, 0);
+      cursor: pointer;
+      transition: 0.1s ease all;
+    }
+    
+    .product > img {
+      align-self: center;
+      justify-self: center;
+      width: 100%;
+    }
+    
+    .title {
+      font-size: 1.1em;
+      margin: 0;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+    
+    .title:hover {
+      font-size: 1.1em;
+      margin: 0;
+      white-space: wrap;
+      overflow: auto;
+      text-overflow: unset;
+    }
+    ` 
+  }
+  
 }
 
 customElements.define('product-item', ProductItem);
