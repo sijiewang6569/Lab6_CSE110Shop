cc8156900b6e08cd03831d023cc3d806c2343c7f
Sijie Wang <56042391+sijiewang6569@users.noreply.github.com>
2021-02-13 22:27:28 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,5 @@
 // Script.js
 
-var cart = [];
-var sizeOfCart = document.getElementById('cart-count');
-
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
   if(localStorage.getItem('items') == null) {
@@ -12,22 +9,24 @@ window.addEventListener('DOMContentLoaded', () => {
   }
   
   if(localStorage.getItem('cart') == null) {
+    let cart = [];
     localStorage.setItem('cart', JSON.stringify(cart));
   }
   cart = JSON.parse(localStorage.getItem('cart'));
 
+  let cartCount = document.getElementById('cart-count');
   if(localStorage.getItem('sizeOfCart') == null) {
-    localStorage.setItem('sizeOfCart', JSON.stringify(sizeOfCart.textContent));
+    localStorage.setItem('sizeOfCart', JSON.stringify(cartCount.textContent));
   }
-  sizeOfCart.textContent = JSON.parse(localStorage.getItem('cartSize'));
+  cartCount.textContent = JSON.parse(localStorage.getItem('sizeOfCart'));
 
   let dataStored = JSON.parse(localStorage.getItem('items'));
-  let array = document.getElementById('array');
+  let productsList = document.getElementById('products-list');
 
   if (dataStored != null){
     for (let i; i < dataStored.length; i++) {
       let curr = new ProductItem(dataStored[i].imagesrc, dataStored[i].title, dataStored[i].price, dataStored[i].id, cart.includes(dataStored[i].id));
-      array.appendChild(curr);
+      productsList.appendChild(curr);
     }
   }
 });
