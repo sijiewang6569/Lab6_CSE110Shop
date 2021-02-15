a2a34ff14e92a6dde1d7ced7ba1d812cf5ef2f6e
Sijie Wang <56042391+sijiewang6569@users.noreply.github.com>
2021-02-14 16:50:30 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,33 @@
 // Script.js
 
+var cart = []
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
-  if(localStorage.getItem('items') == null) {
-    fetch('https://fakestoreapi.com/products')
-      .then(response => response.json())
-      .then(data => localStorage.setItem('items', JSON.stringify(data)));
-  }
+  
+    if (localStorage.getItem("items") == null) {
+        fetch("https://fakestoreapi.com/products")
+            .then(response => response.json())
+            .then(data => localStorage.setItem("items", JSON.stringify(data)));
+    }
 
-  if(localStorage.getItem('cart') == null) {
-    let cart = [];
-    localStorage.setItem('cart', JSON.stringify(cart));
-  }
-  cart = JSON.parse(localStorage.getItem('cart'));
-
-  let cartCount = document.getElementById('cart-count');
-  if(localStorage.getItem('sizeOfCart') == null) {
-    localStorage.setItem('sizeOfCart', JSON.stringify(cartCount.textContent));
-  }
-  cartCount.textContent = JSON.parse(localStorage.getItem('sizeOfCart'));
-
-  let dataStored = JSON.parse(localStorage.getItem('items'));
-  let productsList = document.getElementById('products-list');
-
-  if (dataStored != null){
-    dataStored.forEach(function(index){
-      let curr = document.createElement('product-item');
-      curr = new ProductItem(index.imagesrc, index.title, index.price, index.id, cart.includes(index.id));
-      if (productsList != null){
-        productsList.appendChild(curr);
-      }
-    });
-  }
+    if (localStorage.getItem("cart") == null) {
+        localStorage.setItem("cart", JSON.stringify(cart));
+    }
+    cart = JSON.parse(localStorage.getItem("cart"));
+  
+    let sizeOfCart = document.getElementById("cart-count");
+    if(JSON.parse(localStorage.getItem('count')) == null) {
+        localStorage.setItem('count', JSON.stringify(sizeOfCart.textContent));
+        sizeOfCart.textContent  = JSON.parse(localStorage.getItem('count'))
+    } 
+    else {
+        sizeOfCart.textContent  = JSON.parse(localStorage.getItem('count'));
+    }
+  
+    let storedData = JSON.parse(localStorage.getItem("items"));
+    let list = document.getElementById("product-list");
+    for(let d in storedData){
+        let currItem = new ProductItem(storedData[d].image, storedData[d].title, 
+            storedData[d].price, storedData[d].id, cart.includes(storedData[d].id));
+        list.appendChild(currItem);
+    }
 });
